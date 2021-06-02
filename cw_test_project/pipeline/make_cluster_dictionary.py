c3e111af1a2fd11166d833ee26fc8c23791c7ed7
Chris Williamson <chris.williamson@NESTA-FVFF81VSQ05P.lan>
2021-06-02 17:42:16 +0100

    adding pipeline functions

---
+++ cw_test_project/pipeline/make_cluster_dictionary.py
@@ -0,0 +1,53 @@
+# make_cluster_dictionary.py
+
+# for a given list of occupations,
+# perform a louvain cluster on the subgraph induced by that list
+# return a dictionary of categories to occupations
+
+
+from networkx.algorithms import community, components
+from community import community_louvain
+
+from cw_test_project.pipeline.make_graph import make_graph
+from cw_test_project.utils.invert_dictionary import invert_dictionary
+
+
+full_graph = make_graph()
+
+
+def apply_louvain(graph) -> dict:
+    """Apply Louvain to a graph and form a dictionary from categories to values.
+
+    Returns: A dictionary where the keys are the labels of the Louvain clusters
+    and the values are the nodes in each cluster.
+    """
+    partition = community_louvain.best_partition(graph, randomize=False, random_state=0)
+    return invert_dictionary(partition)
+
+
+split_1level = apply_louvain(full_graph)
+
+
+def split_deeper(dictionary, k=1) -> dict:
+    """Given a dictionary, for each key, apply Louvain to the subgraph induced
+    by the values of that key with respect to the full graph. Repeat k times.
+
+    Returns: A dictionary classifying occupations into a hierarchy with k+1 levels.
+    """
+    split_dictionary = {}
+
+    for i in dictionary.keys():
+        subgraph = full_graph.subgraph(dictionary[i])  # the induced subgraph
+        split_dictionary[i] = apply_louvain(subgraph)
+        if k > 1:
+            split_dictionary[i] = split_deeper(split_dictionary[i], k - 1)
+
+    return split_dictionary
+
+
+split_4levels = split_deeper(split_1level, k=3)
+
+
+# TESTING
+# print(split_4levels[0])
+# print(split_4levels[5][0][0][0])
---
+++ cw_test_project/pipeline/make_graph.py
@@ -0,0 +1,59 @@
+# pipeline - forming a graph
+
+import networkx as nx
+
+from cw_test_project.pipeline.generate_dictionary import generate_dictionary
+
+
+def make_graph() -> nx.Graph:
+    """Make a graph from the dictionary of occupations to skills.
+
+    Returns: A graph where the nodes are occupations,
+    edges exist between nodes if the corresponding occupations share at least one skill,
+    and edge weights are equal to the proportion of skills shared.
+    """
+
+    skills_dict = generate_dictionary()
+
+    # the nodes of the graph are the occupations
+    occupations = list(skills_dict.keys())
+
+    # build the edge set
+    edges = []
+
+    number_of_occs = len(occupations)
+
+    # consider each unique pair of occupations
+    for i in range(0, number_of_occs - 1):
+        for j in range(i + 1, number_of_occs):
+
+            occupation_i = occupations[i]
+            occupation_j = occupations[j]
+
+            # get the skills associated with each occupation
+            i_skills = set(skills_dict[occupation_i])
+            j_skills = set(skills_dict[occupation_j])
+
+            # find the proportion of shared skills
+            intersection = i_skills & j_skills
+            union = i_skills | j_skills
+
+            proportion = len(intersection) / len(union)
+
+            # if this proportion is nonzero, add an edge between these
+            # occupations with weight equal to the proportion
+            if proportion != 0:
+                edges.append((occupation_i, occupation_j, {"weight": proportion}))
+
+    # initialise an empty graph, then add nodes and edges as defined above
+    occupations_graph = nx.Graph()
+
+    occupations_graph.add_nodes_from(occupations)
+    occupations_graph.add_edges_from(edges)
+
+    return occupations_graph
+
+
+# TESTING
+# test_graph = make_graph()
+# print(test_graph.degree('technical director'))
